58972ed3a2c76cb01423b1055ef545898a46577a
Lorenzo Mari <66160739+lumilio@users.noreply.github.com>
2021-10-28 13:55:23 +0200

    Add files via upload

---
+++ asset/js/main.js
@@ -0,0 +1,25 @@
+
+
+
+
+const app = new Vue({
+
+
+    el: '#root',
+
+
+    data: {
+        name: 'ciao',
+        surname: '',
+    },
+
+    methods: {}
+})
+
+
+/* v-ona
+v-model
+v-bind */
+
+
+
